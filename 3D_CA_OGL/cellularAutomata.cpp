8db4f0a884853ba83935f805b057f3261b5b8bdd
m-henke <mthenke2000@gmail.com>
2024-06-18 02:09:48 -0400

    print time update and render takes

--- 3D_CA_OGL/cellularAutomata.cpp
+++ 3D_CA_OGL/cellularAutomata.cpp
@@ -135,11 +135,20 @@ void cellularAutomata::run() {
         
         // render
         // ------
+        auto rs = std::chrono::high_resolution_clock::now();
         renderer->draw(liveCells);
+        auto re = std::chrono::high_resolution_clock::now();
 
         // update
         // ------
+        auto us = std::chrono::high_resolution_clock::now();
         updateGrid();
+        auto ue = std::chrono::high_resolution_clock::now();
+
+        auto rt = std::chrono::duration_cast<std::chrono::milliseconds>(re - rs);
+        auto ut = std::chrono::duration_cast<std::chrono::milliseconds>(ue - us);
+
+        std::cout << "Render Time: " << rt.count() << " Update Time: " << ut.count() << " Live Cells: " << static_cast<int>(liveCells.size()) << '\n';
         
         // glfw: poll IO events (keys pressed/released, mouse moved etc.)
         // -------------------------------------------------------------------------------
